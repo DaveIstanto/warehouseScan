a563e141266e9ade70379b701c0de014bd1af89b
Gabriel Sitorus <sitorusgg@gmail.com>
2021-12-07 21:16:31 -0800

    style_late

---
+++ src/components/SizeButton.tsx
@@ -0,0 +1,9 @@
+export const SizeButton = () => {
+
+    return(
+    <div>
+        {/* MAKE BUTTON HERE */}
+        <button className="font-bold w-50 bg-yellow-200 rounded-lg box-border">Extra small </button>
+        {/* SIZE: EXTRA SMALL */}
+    </div>)
+}
